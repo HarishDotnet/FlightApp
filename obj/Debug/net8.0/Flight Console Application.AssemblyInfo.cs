9a31d5b00fb118cd550078c46b8e51d8116fdc4d
HARISHDOTNET <harishmanikam1437053@gmail.com>
2024-12-08 10:12:58 +0530

    All file commited

--- obj/Debug/net8.0/Flight Console Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flight Console Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flight Console Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ef0194a790941a8c20405ccbc65603f4ff9e25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flight Console Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flight Console Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
